575d4aa023ce66b12495f28ecaeb062eb2e0b800
InfiniteSoul <pascalw98@gmail.com>
2015-06-27 23:10:02 +0200

    [Add] Teilweise #9
    + ID / Benutztername
    + Status
    + Onlinestatus
    + Punkte / Rang

--- Proxer.API/User.cs
+++ Proxer.API/User.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Proxer.API.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Proxer.API
 {
@@ -11,20 +13,45 @@ namespace Proxer.API
     /// </summary>
     public class User
     {
-        private bool checkStatus;
-        private bool checkOnline;
-        private bool checkRang;
-        private bool checkPunkte;
-        private bool checkBeschreibung;
+        private Senpai senpai;
+
+        private string status;
+        private bool online;
+        private string rang;
+        private int punkte;
+
+        private bool checkMain;
+        //private bool checkInfos;
+        //private bool checkFriendList;
+        //private bool checkFriend;
+
+        private Timer checkTimer;
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="name"></param>
         /// <param name="userID"></param>
-        public User(string name, int userID)
+        /// <param name="senpai"></param>
+        public User(string name, int userID, Senpai senpai)
         {
+            this.senpai = senpai;
 
+            this.checkMain = true;
+
+            this.UserName = name;
+            this.ID = userID;
+
+            this.checkTimer = new Timer();
+            this.checkTimer.AutoReset = true;
+            this.checkTimer.Interval = (new TimeSpan(0, 30, 0)).TotalMilliseconds;
+            this.checkTimer.Elapsed += (s, eArgs) =>
+            {
+                this.checkMain = true;
+                //checkInfos = true;
+                //checkFriendList = true;
+                //checkFriend = true;
+            };
         }
 
         /// <summary>
@@ -38,22 +65,106 @@ namespace Proxer.API
         /// <summary>
         /// 
         /// </summary>
-        public string Status { get; private set; }
+        public string Status
+        {
+            get
+            {
+                if (checkMain) getMainInfo();
+                return this.status;
+            }
+        }
         /// <summary>
         /// 
         /// </summary>
-        public bool Online { get; private set; }
+        public bool Online
+        {
+            get
+            {
+                if (checkMain) getMainInfo();
+                return this.online;
+            }
+        }
         /// <summary>
         /// 
         /// </summary>
-        public string Rang { get; private set; }
+        public string Rang
+        {
+            get
+            {
+                if (checkMain) getMainInfo();
+                return this.rang;
+            }
+        }
         /// <summary>
         /// 
         /// </summary>
-        public int Punkte { get; private set; }
+        public int Punkte
+        {
+            get
+            {
+                if (checkMain) getMainInfo();
+                return this.punkte;
+            }
+        }
+
+
+        /// <summary>
+        /// (vorläufig, nicht ausführlich getestet)
+        /// </summary>
+        public async void getMainInfo()
+        {
+            HtmlAgilityPack.HtmlDocument lDocument = new HtmlAgilityPack.HtmlDocument();
+            string lResponse = (await HttpUtility.GetWebRequestResponseAsync("https://proxer.me/user/" + this.ID + "/overview?format=raw", this.senpai.LoginCookies)).Replace("</link>", "").Replace("\n", "");
+
+            lDocument.LoadHtml(lResponse);
+
+            if (lDocument.ParseErrors.Count() == 0)
+            {
+                HtmlAgilityPack.HtmlNodeCollection lProfileNodes = lDocument.DocumentNode.SelectNodes("//table[@class='profile']");
+
+                if (lProfileNodes != null)
+                {
+                    this.punkte = Convert.ToInt32(Utility.Utility.GetTagContents(lProfileNodes[0].FirstChild.InnerText, "Summe: ", " - ")[0]);
+                    this.rang = Utility.Utility.GetTagContents(lProfileNodes[0].FirstChild.InnerText, this.punkte.ToString() + " - ", "[?]")[0];
+                    this.online = lProfileNodes[0].ChildNodes[1].InnerText.Equals("Status Online");
+                    this.status = lProfileNodes[0].ChildNodes[6].InnerText;
+
+                    if (this.UserName.Equals(""))
+                    {
+                        this.UserName = lDocument.DocumentNode.SelectNodes("//div[@id='pageMetaAjax']")[0].InnerText.Split(' ')[1];
+                    }
+                }
+            }
+        }
+
         /// <summary>
         /// 
         /// </summary>
-        public string Beschreibung { get; private set; }
+        /// <param name="id"></param>
+        /// <param name="loginCookies"></param>
+        /// <returns></returns>
+        public static async Task<string> getUNameFromID(int id, System.Net.CookieContainer loginCookies)
+        {
+            HtmlAgilityPack.HtmlDocument lDocument = new HtmlAgilityPack.HtmlDocument();
+            string lResponse = await HttpUtility.GetWebRequestResponseAsync("https://proxer.me/user/" + id + "/overview?format=raw", loginCookies);
+
+            lDocument.LoadHtml(lResponse);
+
+            if (lDocument.ParseErrors.Count() == 0)
+            {
+                try
+                {
+                    return lDocument.DocumentNode.SelectNodes("//div[@id='pageMetaAjax']")[0].InnerText.Split(' ')[1];
+                }
+                catch (Exception)
+                {
+                    return "";
+                }
+            }
+            else
+            {
+                return "";
+            }
+        }
     }
 }
