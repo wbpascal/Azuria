e89850a3419f038e80234a10b7b3351df3d76098
InfiniteSoul <pascalw98@gmail.com>
2015-08-27 19:23:32 +0200

    [Edit] User
    +jetzt muss erst initUser() aufgerufen werden, bevor die Eigenschaften
    einen Wert haben. Deswegen werden die Eigenschaften auch nicht mehr alle
    15 Minuten aktualisiert, dies muss jetzt manuell geschehen.
    +travis.yml entfernt (benutze kein Travis CI mehr)

--- Proxer.API/User.cs
+++ Proxer.API/User.cs
@@ -18,21 +18,7 @@ namespace Proxer.API
         /// </summary>
         public static User System = new User("System", -1, new Senpai());
 
-        private Senpai senpai;
-
-        private string status;
-        private bool online;
-        private string rang;
-        private int punkte;
-        private List<User> freunde;
-        private Uri avatar;
-        private string info;
-
-        private bool checkMain;
-        private bool checkFriends;
-        private bool checkInfo;
-
-        private Timer checkTimer;
+        private readonly Senpai senpai;
 
         /// <summary>
         /// 
@@ -40,27 +26,13 @@ namespace Proxer.API
         /// <param name="name"></param>
         /// <param name="userID"></param>
         /// <param name="senpai"></param>
-        public User(string name, int userID, Senpai senpai)
+        internal User(string name, int userID, Senpai senpai)
         {
             this.senpai = senpai;
-
-            this.checkMain = true;
-            this.checkFriends = true;
-            this.checkInfo = true;
 
             this.UserName = name;
             this.ID = userID;
             this.Avatar = new Uri("https://proxer.me/components/com_comprofiler/plugin/templates/default/images/avatar/nophoto_n.png");
-
-            this.checkTimer = new Timer();
-            this.checkTimer.AutoReset = true;
-            this.checkTimer.Interval = (new TimeSpan(0, 5, 0)).TotalMilliseconds;
-            this.checkTimer.Elapsed += (s, eArgs) =>
-            {
-                this.checkMain = true;
-                this.checkFriends = true;
-                this.checkInfo = true;
-            };
         }
         /// <summary>
         /// 
@@ -69,29 +41,29 @@ namespace Proxer.API
         /// <param name="userID"></param>
         /// <param name="avatar"></param>
         /// <param name="senpai"></param>
-        public User(string name, int userID, Uri avatar, Senpai senpai)
+        internal User(string name, int userID, Uri avatar, Senpai senpai)
         {
             this.senpai = senpai;
-
-            this.checkMain = true;
-            this.checkFriends = true;
-            this.checkInfo = true;
 
             this.UserName = name;
             this.ID = userID;
             if (avatar != null) this.Avatar = avatar;
             else this.Avatar = new Uri("https://proxer.me/components/com_comprofiler/plugin/templates/default/images/avatar/nophoto_n.png");
-
-            this.checkTimer = new Timer();
-            this.checkTimer.AutoReset = true;
-            this.checkTimer.Interval = (new TimeSpan(0, 5, 0)).TotalMilliseconds;
-            this.checkTimer.Elapsed += (s, eArgs) =>
-            {
-                this.checkMain = true;
-                this.checkFriends = true;
-                this.checkInfo = true;
-            };
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="senpai"></param>
+        public User(int userID, Senpai senpai)
+        {
+            this.senpai = senpai;
+
+            this.UserName = User.getUNameFromID(userID, senpai);
+            this.ID = userID;
+            this.Avatar = new Uri("https://proxer.me/components/com_comprofiler/plugin/templates/default/images/avatar/nophoto_n.png");
+        }
+
 
         /// <summary>
         /// 
@@ -104,104 +76,52 @@ namespace Proxer.API
         /// <summary>
         /// 
         /// </summary>
-        public string Status
-        {
-            get
-            {
-                if (checkMain) this.getMainInfo();
-                return this.status;
-            }
-        }
+        public string Status { get; private set; }
         /// <summary>
         /// 
         /// </summary>
-        public bool Online
-        {
-            get
-            {
-                if (checkMain) this.getMainInfo();
-                return this.online;
-            }
-        }
+        public bool Online { get; private set; }
         /// <summary>
         /// 
         /// </summary>
-        public string Rang
-        {
-            get
-            {
-                if (checkMain) this.getMainInfo();
-                return this.rang;
-            }
-        }
+        public string Rang { get; private set; }
         /// <summary>
         /// 
         /// </summary>
-        public int Punkte
-        {
-            get
-            {
-                if (checkMain) this.getMainInfo();
-                return this.punkte;
-            }
-        }
+        public int Punkte { get; private set; }
         /// <summary>
         /// 
         /// </summary>
-        public List<User> Freunde
-        {
-            get
-            {
-                if (this.checkFriends) this.getFriends();
-                return this.freunde;
-            }
-        }
+        public List<User> Freunde { get; private set; }
         /// <summary>
         /// 
         /// </summary>
-        public Uri Avatar
-        {
-            get
-            {
-                if (this.checkFriends) this.getMainInfo();
-                return this.avatar;
-            }
-            private set
-            {
-                if (value != null) this.avatar = value;
-            }
-        }
+        public Uri Avatar { get; private set; }
         /// <summary>
         /// 
         /// </summary>
-        public string Info
-        {
-            get
-            {
-                if (this.checkInfo) this.getInfos();
-                return this.info;
-            }
-            private set
-            {
-                if (value != null) this.info = value;
-            }
-        }
-
+        public string Info { get; private set; }
 
         /// <summary>
-        /// (vorläufig, nicht ausführlich getestet)
+        /// 
         /// </summary>
+        public void initUser()
+        {
+            this.getMainInfo();
+            this.getFriends();
+            this.getInfos();
+        }
+        
+
         private void getMainInfo()
         {
             if (this.ID == -1)
             {
-                this.status = "";
-                this.online = false;
-                this.rang = "";
-                this.punkte = -1;
+                this.Status = "";
+                this.Online = false;
+                this.Rang = "";
+                this.Punkte = -1;
                 this.Avatar = new Uri("https://proxer.me/components/com_comprofiler/plugin/templates/default/images/avatar/nophoto_n.png");
-
-                this.checkMain = false;
             }
             else if (senpai.LoggedIn)
             {
@@ -220,19 +140,17 @@ namespace Proxer.API
 
                             if (lProfileNodes != null)
                             {
-                                this.Avatar = new Uri("https://proxer.me" + lProfileNodes[0].ParentNode.ChildNodes[1].ChildNodes[0].Attributes["src"].Value);
-                                this.punkte = Convert.ToInt32(Utilities.Utility.GetTagContents(lProfileNodes[0].FirstChild.InnerText, "Summe: ", " - ")[0]);
-                                this.rang = Utilities.Utility.GetTagContents(lProfileNodes[0].FirstChild.InnerText, this.punkte.ToString() + " - ", "[?]")[0];
-                                this.online = lProfileNodes[0].ChildNodes[1].InnerText.Equals("Status Online");
-                                if (lProfileNodes[0].ChildNodes.Count() == 7) this.status = lProfileNodes[0].ChildNodes[6].InnerText;
-                                else this.status = "";
+                                this.Avatar = new Uri("https://proxer.me" + lProfileNodes[0].ParentNode.ParentNode.ChildNodes[1].ChildNodes[0].Attributes["src"].Value);
+                                this.Punkte = Convert.ToInt32(Utilities.Utility.GetTagContents(lProfileNodes[0].FirstChild.InnerText, "Summe: ", " - ")[0]);
+                                this.Rang = Utilities.Utility.GetTagContents(lProfileNodes[0].FirstChild.InnerText, this.Punkte.ToString() + " - ", "[?]")[0];
+                                this.Online = lProfileNodes[0].ChildNodes[1].InnerText.Equals("Status Online");
+                                if (lProfileNodes[0].ChildNodes.Count() == 7) this.Status = lProfileNodes[0].ChildNodes[6].InnerText;
+                                else this.Status = "";
 
                                 if (this.UserName.Equals(""))
                                 {
                                     this.UserName = lDocument.DocumentNode.SelectNodes("//div[@id='pageMetaAjax']")[0].InnerText.Split(' ')[1];
                                 }
-
-                                this.checkMain = false;
                             }
                         }
                     }
@@ -243,16 +161,11 @@ namespace Proxer.API
                 }
             }
         }
-        /// <summary>
-        /// 
-        /// </summary>
         private void getFriends()
         {
             if (this.ID == -1)
             {
-                this.freunde = new List<User>();
-
-                this.checkFriends = false;
+                this.Freunde = new List<User>();
             }
             else if (senpai.LoggedIn)
             {
@@ -260,12 +173,14 @@ namespace Proxer.API
                 string lResponse;
                 HtmlAgilityPack.HtmlDocument lDocument = new HtmlAgilityPack.HtmlDocument();
 
-                this.freunde = new List<User>();
+                this.Freunde = new List<User>();
 
                 while (!(lResponse = (HttpUtility.GetWebRequestResponse("https://proxer.me/user/" + this.ID + "/connections/" + lSeite + "?format=raw", this.senpai.LoginCookies)).Replace("</link>", "").Replace("\n", "").Replace("\t", "")).Contains("Dieser Benutzer hat bisher keine Freunde"))
                 {
                     if (Utilities.Utility.checkForCorrectResponse(lResponse, senpai.ErrHandler))
                     {
+                        if (lResponse.Equals("<div class=\"inner\"><h3>Du hast keine Berechtigung um diese Seite zu betreten.</h3></div>")) break;
+
                         try
                         {
                             lDocument.LoadHtml(lResponse);
@@ -283,7 +198,7 @@ namespace Proxer.API
                                     {
                                         string lUsername = curFriendNode.ChildNodes[2].InnerText;
                                         int lID = Convert.ToInt32(curFriendNode.Attributes["id"].Value.Substring("entry".Length));
-                                        this.freunde.Add(new User(lUsername, lID, this.senpai));
+                                        this.Freunde.Add(new User(lUsername, lID, this.senpai));
                                     }
                                 }
                             }
@@ -296,20 +211,13 @@ namespace Proxer.API
                         }
                     }
                 }
-
-                this.checkFriends = false;
             }
         }
-        /// <summary>
-        /// 
-        /// </summary>
         private void getInfos()
         {
             if (this.ID == -1)
             {
                 this.Info = "";
-
-                this.checkInfo = false;
             }
             else if (senpai.LoggedIn)
             {
@@ -329,8 +237,6 @@ namespace Proxer.API
                             if (lProfileNodes != null)
                             {
                                 this.Info = lProfileNodes[0].ChildNodes[10].InnerText;
-
-                                this.checkInfo = false;
                             }
                         }
                     }
@@ -342,15 +248,34 @@ namespace Proxer.API
 
             }
         }
+
+
+        /*
+         * -----------------------------------
+         * --------Statische Methoden---------
+         * -----------------------------------
+        */
+
+        /// <summary>
+        /// Überprüft, ob zwei Benutzter Freunde sind.
+        /// </summary>
+        /// <param name="user1"></param>
+        /// <param name="user2"></param>
+        /// <returns></returns>
+        public static bool isUserFriendOf(User user1, User user2)
+        {
+            return user1.Freunde.Any(item => item.ID == user2.ID);
+        }
         /// <summary>
         /// 
         /// </summary>
         /// <param name="id"></param>
+        /// <param name="senpai"></param>
         /// <returns></returns>
-        public string getUNameFromID(int id)
+        public static string getUNameFromID(int id, Senpai senpai)
         {
             HtmlAgilityPack.HtmlDocument lDocument = new HtmlAgilityPack.HtmlDocument();
-            string lResponse = HttpUtility.GetWebRequestResponse("https://proxer.me/user/" + id + "/overview?format=raw", this.senpai.LoginCookies);
+            string lResponse = HttpUtility.GetWebRequestResponse("https://proxer.me/user/" + id + "/overview?format=raw", senpai.LoginCookies);
 
             if (Utilities.Utility.checkForCorrectResponse(lResponse, senpai.ErrHandler))
             {
@@ -371,29 +296,11 @@ namespace Proxer.API
                 }
                 catch (Exception)
                 {
-                    this.senpai.ErrHandler.add(lResponse);
+                    senpai.ErrHandler.add(lResponse);
                 }
             }
 
             return "";
-        }
-
-
-        /*
-         * -----------------------------------
-         * --------Statische Methoden---------
-         * -----------------------------------
-        */
-
-        /// <summary>
-        /// Überprüft, ob zwei Benutzter Freunde sind.
-        /// </summary>
-        /// <param name="user1"></param>
-        /// <param name="user2"></param>
-        /// <returns></returns>
-        public static bool isUserFriendOf(User user1, User user2)
-        {
-            return user1.Freunde.Any(item => item.ID == user2.ID);
         }
     }
 }
