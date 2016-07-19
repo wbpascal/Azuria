4b09dcb2d58312987a67fe4ae1fb2a4ba2c766da
InfiniteSoul <infinitesoul155@gmail.com>
2016-07-20 00:40:40 +0200

    Test credentials removal

--- Azuria.Test/Credentials.cs
+++
@@ -1,9 +0,0 @@
-namespace Azuria.Test
-{
-    internal static class Credentials
-    {
-        public const string Username = "";
-        public const string Password = "";
-        public const string ApiKey = "";
-    }
-}
