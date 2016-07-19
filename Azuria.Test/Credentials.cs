05c55842d047316c0adb6332be3803e81752bbb9
InfiniteSoul <InfiniteSoul@users.noreply.github.com>
2016-07-20 00:07:58 +0200

    Create template for credentials file (Azuria.Test)

---
+++ Azuria.Test/Credentials.cs
@@ -0,0 +1,9 @@
+namespace Azuria.Test
+{
+    internal static class Credentials
+    {
+        public const string Username = "";
+        public const string Password = "";
+        public const string ApiKey = "";
+    }
+}
