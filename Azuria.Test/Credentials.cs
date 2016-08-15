e2dcbcb10b7123e0fdb84d126fa104779c318ff7
InfiniteSoul <infinitesoul155@gmail.com>
2016-08-15 22:54:37 +0200

    Remove Azuria.Test for real now #47

--- Azuria.Test/Credentials.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Azuria.Test
-{
-    internal static class Credentials
-    {
-        public const string ApiKey = "";
-        public const string Password = "";
-        public const string Username = "";
-    }
-}
