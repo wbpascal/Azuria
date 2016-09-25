83739240ba5ca1ff68d14208cea783a93922b21c
InfiniteSoul <infinitesoul155@gmail.com>
2016-09-25 18:03:20 +0200

    Fix some error cases in SubRatingsConverter

--- Azuria/Api/v1/Converters/SubRatingsConverter.cs
+++ Azuria/Api/v1/Converters/SubRatingsConverter.cs
@@ -33,6 +33,8 @@ namespace Azuria.Api.v1.Converters
         {
             try
             {
+                if (string.IsNullOrEmpty(reader.Value.ToString()) || reader.Value.ToString().Equals("[]"))
+                    return new Dictionary<RatingCategory, int>();
                 return JsonConvert.DeserializeObject<Dictionary<string, int>>(reader.Value.ToString())
                     .ToDictionary(
                         keyValuePair => (RatingCategory) Enum.Parse(typeof(RatingCategory), keyValuePair.Key, true),
