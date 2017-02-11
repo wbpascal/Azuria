f9d455050a3aaf5d2f9b258b8783a00cfb24d20d
InfiniteSoul <infinitesoul155@gmail.com>
2017-02-11 21:07:27 +0100

    Add docs for 'MediaRequestBuilder'

--- Azuria/Api/v1/RequestBuilder/MediaRequestBuilder.cs
+++ Azuria/Api/v1/RequestBuilder/MediaRequestBuilder.cs
@@ -10,25 +10,28 @@ namespace Azuria.Api.v1.RequestBuilder
         #region Methods
 
         /// <summary>
-        /// Creates an <see cref="ApiRequest" /> instance that...
+        /// Creates an <see cref="ApiRequest" /> instance that returns an array of all current headers.
         /// 
         /// Api permissions required:
         /// * Media - Level 0
         /// </summary>
-        /// <returns>An instance of <see cref="ApiRequest" /> that returns...</returns>
+        /// <returns>An instance of <see cref="ApiRequest" /> that returns an array of headers.</returns>
         public static ApiRequest<HeaderDataModel[]> GetHeaderList()
         {
             return ApiRequest<HeaderDataModel[]>.Create(new Uri($"{ApiConstants.ApiUrlV1}/media/headerlist"));
         }
 
         /// <summary>
-        /// Creates an <see cref="ApiRequest" /> instance that...
+        /// Creates an <see cref="ApiRequest" /> instance that returns a random header for an optional specified style.
         /// 
         /// Api permissions required:
         /// * Media - Level 0
         /// </summary>
-        /// <param name="style"></param>
-        /// <returns>An instance of <see cref="ApiRequest" /> that returns...</returns>
+        /// <param name="style">
+        /// Optional. The style of the returned header. Possible values: "gray", "black", "old_blue", "pantsu".
+        /// Default: "gray"
+        /// </param>
+        /// <returns>An instance of <see cref="ApiRequest" /> that returns a header.</returns>
         public static ApiRequest<HeaderDataModel> GetRandomHeader(string style = "gray")
         {
             return ApiRequest<HeaderDataModel>.Create(
