474edc21bc2fd4aa16c5e60d588d0fdba6f49990
InfiniteSoul <infinitesoul155@gmail.com>
2017-02-27 20:37:43 +0100

    Remove c# 7 features unitl appveyor supports msbuild 15 Shouldn't be too long because vs 17 officially releases march 7th alongside the new msbuild version

--- Azuria/UserInfo/ControlPanel/BookmarkEnumerator.cs
+++ Azuria/UserInfo/ControlPanel/BookmarkEnumerator.cs
@@ -26,37 +26,37 @@ namespace Azuria.UserInfo.ControlPanel
 
         #region Methods
 
+        private Bookmark<T> GetBookmarkObject(BookmarkDataModel dataModel)
+        {
+            if (typeof(T) != typeof(Manga) && dataModel.EntryType == MediaEntryType.Anime)
+                return new Bookmark<T>((IMediaContent<T>) new Episode(dataModel), dataModel.BookmarkId,
+                    this._controlPanel);
+            if (typeof(T) != typeof(Anime) && dataModel.EntryType == MediaEntryType.Manga)
+                return new Bookmark<T>((IMediaContent<T>) new Chapter(dataModel), dataModel.BookmarkId,
+                    this._controlPanel);
+
+            return null;
+        }
+
+        private static string GetCategoryString()
+        {
+            if (typeof(T) == typeof(Anime) || typeof(T) == typeof(Manga))
+                return typeof(T).GetTypeInfo().Name.ToLowerInvariant();
+            return string.Empty;
+        }
+
         /// <inheritdoc />
         protected override async Task<IProxerResult<IEnumerable<Bookmark<T>>>> GetNextPage(int nextPage)
         {
             ProxerApiResponse<BookmarkDataModel[]> lResult = await RequestHandler.ApiRequest(
-                    UcpRequestBuilder.GetReminder(this._senpai, GetCategory(), nextPage, ResultsPerPage))
+                    UcpRequestBuilder.GetReminder(this._senpai, GetCategoryString(), nextPage, ResultsPerPage))
                 .ConfigureAwait(false);
             if (!lResult.Success || lResult.Result == null)
                 return new ProxerResult<IEnumerable<Bookmark<T>>>(lResult.Exceptions);
             BookmarkDataModel[] lData = lResult.Result;
 
             return new ProxerResult<IEnumerable<Bookmark<T>>>((from bookmarkDataModel in lData
-                select GetBookmark(bookmarkDataModel)).Where(bookmark => bookmark != null));
-
-            string GetCategory()
-            {
-                if (typeof(T) == typeof(Anime) || typeof(T) == typeof(Manga))
-                    return typeof(T).GetTypeInfo().Name.ToLowerInvariant();
-                return string.Empty;
-            }
-
-            Bookmark<T> GetBookmark(BookmarkDataModel dataModel)
-            {
-                if (typeof(T) != typeof(Manga) && dataModel.EntryType == MediaEntryType.Anime)
-                    return new Bookmark<T>((IMediaContent<T>) new Episode(dataModel), dataModel.BookmarkId,
-                        this._controlPanel);
-                if (typeof(T) != typeof(Anime) && dataModel.EntryType == MediaEntryType.Manga)
-                    return new Bookmark<T>((IMediaContent<T>) new Chapter(dataModel), dataModel.BookmarkId,
-                        this._controlPanel);
-
-                return null;
-            }
+                select this.GetBookmarkObject(bookmarkDataModel)).Where(bookmark => bookmark != null));
         }
 
         #endregion
