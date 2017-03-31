a5350ba129c1560d74bab38d9eb3780324c2ef59
InfiniteSoul <infinitesoul155@gmail.com>
2017-03-31 23:24:20 +0200

    Publish nuget packages through appveyor + include symbols

--- AppveyorPack.ps1
+++ AppveyorPack.ps1
@@ -10,19 +10,7 @@ function NugetPack([bool] $pre)
         echo "Creating stable package..."
     }
     
-    nuget pack Azuria.nuspec -version "$($env:GitVersion_FullSemVer)"
-}
-
-function MygetPush 
-{
-    if ($env:myget_key)
-    {
-        Get-ChildItem $PSScriptRoot -Filter *.nupkg | 
-        Foreach-Object {
-            nuget push $_ $env:myget_key -Source https://www.myget.org/F/infinitesoul/api/v2/package
-        }
-    }
+    nuget pack Azuria.nuspec -symbols -version "$($env:GitVersion_FullSemVer)"
 }
 
 NugetPack ($env:APPVEYOR_REPO_TAG -eq "false")
-if ($env:APPVEYOR_REPO_BRANCH -eq "master") { MygetPush }
