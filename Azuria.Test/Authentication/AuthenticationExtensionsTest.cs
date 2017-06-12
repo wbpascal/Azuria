68b0c3167c608fee9ffdaa3470b0e5805d4f46f9
InfiniteSoul <infinitesoul155@gmail.com>
2017-06-12 20:27:52 +0200

    Add tests for 'AuthenticationExtensions'

---
+++ Azuria.Test/Authentication/AuthenticationExtensionsTest.cs
@@ -0,0 +1,56 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Azuria.Authentication;
+using Azuria.ErrorHandling;
+using Azuria.Test.Core;
+using Moq;
+using Xunit;
+
+namespace Azuria.Test.Authentication
+{
+    public class AuthenticationExtensionsTest
+    {
+        private const string Username = "username";
+        private const string Password = "password";
+        private const string SecretKey = "ACGES1";
+        private readonly CancellationToken _cancellationToken = new CancellationToken();
+        private readonly Mock<ILoginManager> _loginMangerMock;
+        private readonly IProxerClient _proxerClient;
+
+        public AuthenticationExtensionsTest()
+        {
+            this._loginMangerMock = new Mock<ILoginManager>();
+            this._loginMangerMock
+                .Setup(manager => manager.PerformLoginAsync(Username, Password, SecretKey, this._cancellationToken))
+                .ReturnsAsync(new ProxerResult());
+            this._loginMangerMock
+                .Setup(manager => manager.PerformLogoutAsync(this._cancellationToken))
+                .ReturnsAsync(new ProxerResult());
+            this._proxerClient = ProxerClient.Create(
+                new char[32], options => options.WithCustomLoginManager(context => this._loginMangerMock.Object)
+            );
+        }
+
+        [Fact]
+        public async Task PerformLoginAsyncCalledLoginManagerMethodTest()
+        {
+            IProxerResult lResult =
+                await this._proxerClient.LoginAsync("username", "password", SecretKey, this._cancellationToken);
+            Assert.True(lResult.Success);
+            Assert.Empty(lResult.Exceptions);
+            this._loginMangerMock.Verify(
+                manager => manager.PerformLoginAsync(Username, Password, SecretKey, this._cancellationToken),
+                Times.Once
+            );
+        }
+
+        [Fact]
+        public async Task PerformLogoutAsyncCalledLoginManagerMethodTest()
+        {
+            IProxerResult lResult = await this._proxerClient.LogoutAsync(this._cancellationToken);
+            Assert.True(lResult.Success);
+            Assert.Empty(lResult.Exceptions);
+            this._loginMangerMock.Verify(manager => manager.PerformLogoutAsync(this._cancellationToken), Times.Once);
+        }
+    }
+}
