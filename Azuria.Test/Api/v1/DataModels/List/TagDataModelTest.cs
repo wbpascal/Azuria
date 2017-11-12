ff7af6994b52ffa4707490a589c80157d57d2c53
InfiniteSoul <infinitesoul155@gmail.com>
2017-11-12 21:26:48 +0100

    Add tests for list data models

---
+++ Azuria.Test/Api/v1/DataModels/List/TagDataModelTest.cs
@@ -0,0 +1,16 @@
+﻿using Azuria.Api.v1.DataModels.List;
+using Azuria.Test.Core;
+using NUnit.Framework;
+
+namespace Azuria.Test.Api.v1.DataModels.List
+{
+    public class TagDataModelTest : DataModelsTestBase<TagDataModel>
+    {
+        [Test]
+        public void ConvertTest()
+        {
+            string lJson = ResponseSetup.FileResponses["list_getentrysearch.json"];
+            //TODO: Complete this
+        }
+    }
+}
