1516c48f84b75fbccd0671e187f601575b2cbeca
InfiniteSoul <infinitesoul155@gmail.com>
2017-11-12 21:50:48 +0100

    Add tests for messenger data models

---
+++ Azuria.Test/Api/v1/DataModels/Messenger/ConstantsDataModelTest.cs
@@ -0,0 +1,30 @@
+﻿using Azuria.Api.v1.DataModels.Messenger;
+using Azuria.ErrorHandling;
+using Azuria.Test.Core;
+using NUnit.Framework;
+
+namespace Azuria.Test.Api.v1.DataModels.Messenger
+{
+    public class ConstantsDataModelTest : DataModelsTestBase<ConstantsDataModel>
+    {
+        [Test]
+        public void ConvertTest()
+        {
+            string lJson = ResponseSetup.FileResponses["messenger_getconstants.json"];
+            ProxerApiResponse<ConstantsDataModel> lResponse = this.Convert(lJson);
+            Assert.AreEqual(BuildDataModel(), lResponse.Result);
+        }
+
+        private static ConstantsDataModel BuildDataModel()
+        {
+            return new ConstantsDataModel
+            {
+                ConferencesPerPage = 48,
+                MaxCharactersPerMessage = 65_000,
+                MaxCharactersTopic = 32,
+                MaxUsersPerConference = 100,
+                MessagesPerPage = 30
+            };
+        }
+    }
+}
---
+++ Azuria.Test/Api/v1/DataModels/Messenger/MessageDataModelTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Azuria.Api.v1.DataModels.Messenger;
+using Azuria.Enums.Messenger;
+using Azuria.ErrorHandling;
+using Azuria.Helpers;
+using Azuria.Test.Core;
+using NUnit.Framework;
+
+namespace Azuria.Test.Api.v1.DataModels.Messenger
+{
+    public class MessageDataModelTest : DataModelsTestBase<MessageDataModel>
+    {
+        [Test]
+        public void ConvertTest()
+        {
+            string lJson = ResponseSetup.FileResponses["messenger_getmessages.json"];
+            ProxerApiResponse<MessageDataModel[]> lResponse = this.ConvertArray(lJson);
+            Assert.AreEqual(2, lResponse.Result.Length);
+            Assert.AreEqual(BuildDataModel(), lResponse.Result.First());
+        }
+
+        private static MessageDataModel BuildDataModel()
+        {
+            return new MessageDataModel
+            {
+                ConferenceId = 124536,
+                MessageAction = MessageAction.NoAction,
+                MessageContent = "Pro Klasse gezählt auch",
+                MessageId = 4993930,
+                MessageTimeStamp = DateTimeHelpers.UnixTimeStampToDateTime(1473602860),
+                SenderDevice = "default",
+                SenderUserId = 121658,
+                SenderUsername = "RubyDerBoss"
+            };
+        }
+    }
+}
