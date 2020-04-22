1ab2ce9cd48631d8b6996ccd9d514d0b87af9dfc
Pascal Wiedenbeck <infinitesoul155@gmail.com>
2020-04-22 21:01:30 +0200

    Add tests for middleware pipeline

---
+++ Azuria.Test/Middleware/TestMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Azuria.ErrorHandling;
+using Azuria.Middleware;
+using Azuria.Requests.Builder;
+
+namespace Azuria.Test.Middleware
+{
+    class TestMiddleware : IMiddleware
+    {
+        private readonly object _obj;
+
+        public TestMiddleware(object obj)
+        {
+            this._obj = obj;
+        }
+
+        public Task<IProxerResult> Invoke(IRequestBuilder request, MiddlewareAction next, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult((IProxerResult) new ProxerApiResponse {Success = true, Message = "TestMiddleware"});
+        }
+
+        public Task<IProxerResult<T>> InvokeWithResult<T>(IRequestBuilderWithResult<T> request, MiddlewareAction<T> next,
+            CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult((IProxerResult<T>) new ProxerApiResponse<T> {Success = true, Result = (T) this._obj, Message = "TestMiddleware"});
+        }
+    }
+}
